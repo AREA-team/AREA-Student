3f0e43b6bec5cebe46a3d3510316cde1ad41856d
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2020-12-02 19:15:10 +0500

    Tasks submitting realized

--- mainwindow.py
+++ mainwindow.py
@@ -1,16 +1,16 @@
-import sys
-
 from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QListWidgetItem
 
 from redefined_widgets import Window
 from UI.mainwindow_ui import Ui_MainWindow
-from tools import ConnectThread, Server
+from tools import ConnectThread, Server, Task
+from task_dialog import TaskDialog
 
 
 class MainWindow(Window, Ui_MainWindow):
-    def __init__(self, task_table, personal_data):
+    def __init__(self, task_table, personal_data, service, spreadsheet_id):
         super(MainWindow, self).__init__(self.setupUi)
+        self.spreadsheet_id = spreadsheet_id
+        self.service = service
         self.db: Server
         self.db = None
         self.good_conn = False
@@ -21,6 +21,7 @@ class MainWindow(Window, Ui_MainWindow):
         self.subjects = None
         self.tasks = None
         self.tasks_deadlines = None
+        self.index = None
         self.main_label.setText(f'{self.first_name} {self.last_name} '
                                 f'{self.class_number + self.class_letter}')
         self.header.setHeight(30)
@@ -35,34 +36,71 @@ class MainWindow(Window, Ui_MainWindow):
         self.parse_task_table()
         self.update_calendar()
         self.calendar.selectionChanged.connect(self.update_calendar)
+        self.add_task_btn.clicked.connect(self.add_task)
+        self.change_task_btn.clicked.connect(self.change_task)
+        self.submit_task_btn.clicked.connect(self.submit_task)
+
+    def add_task(self):
+        self.service.batchUpdate(spreadsheetId=self.spreadsheet_id,
+                                 body={"requests": [
+                                     {
+                                         "insertDimension": {
+                                             "range": {
+                                                 "dimension": "COLUMNS",
+                                                 "startIndex": 2,
+                                                 "endIndex": 4
+                                             },
+                                             "inheritFromBefore": True
+                                         }
+                                     }]}).execute()
+
+    def change_task(self):
+        pass
+
+    def submit_task(self):
+        if self.tasks_list.selectedItems():
+            tw = TaskDialog(task=self.tasks_list.selectedItems()[0],
+                            service=self.service,
+                            task_table=self.task_table,
+                            student_index=self.index,
+                            spreadsheet_id=self.spreadsheet_id
+                            )
+            tw.show()
+            tw.exec()
+            self.task_table = self.service.values().get(spreadsheetId=self.spreadsheet_id,
+                                                        range='Контроль сдачи!A:DT').execute()
+            self.parse_task_table()
+            self.update_calendar()
 
     def parse_task_table(self):
         self.subjects = self.task_table['values'][0][3:]
         for i in range(1, len(self.subjects)):
             if not self.subjects[i]:
-                self.subjects[i] = self.subjects[i-1]
+                self.subjects[i] = self.subjects[i - 1]
         self.tasks = self.task_table['values'][2][3:]
         self.tasks_deadlines = self.task_table['values'][3][3:]
 
         for index, first_name, last_name, *deadlines in self.task_table['values'][4:]:
             if first_name == self.first_name and last_name == self.last_name:
                 self.deadlines = deadlines
+                self.index = index
 
     def update_calendar(self):
         self.tasks_list.clear()
         current_subject = None
         year, month, day = self.calendar.selectedDate().toPyDate().__str__().split('-')
         for tdi in range(len(self.tasks_deadlines)):
-            if month == self.tasks_deadlines[tdi][3:5] and day == self.tasks_deadlines[tdi][:2] and\
-                    not self.deadlines[tdi]:
+            if month == self.tasks_deadlines[tdi][3:5] and day == self.tasks_deadlines[tdi][:2] \
+                    and not self.deadlines[tdi]:
                 subject = self.subjects[tdi]
                 task = self.tasks[tdi]
-                time = self.tasks_deadlines[tdi][6:]
+                hour, minute = self.tasks_deadlines[tdi][6:].split(':')
                 if not current_subject or current_subject != subject:
-                    self.tasks_list.addItem(QListWidgetItem(f'{subject}:\n    {time} | {task}'))
+                    self.tasks_list.addItem(
+                        Task(year, month, day, hour, minute, subject, task, True))
                     current_subject = subject
                 else:
-                    self.tasks_list.addItem(QListWidgetItem(f'    {time} | {task}'))
+                    self.tasks_list.addItem(Task(year, month, day, hour, minute, subject, task))
                 self.update()
                 self.repaint()
 
@@ -83,10 +121,3 @@ class MainWindow(Window, Ui_MainWindow):
         self.header.conn_state.setIcon(QIcon(QPixmap('System Files/good_connection.png')))
         self.setEnabled(True)
         self.good_conn = True
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    wnd = MainWindow(1, ('Никулин', 'Василий', '10', 'А'))
-    wnd.show()
-    sys.exit(app.exec())
